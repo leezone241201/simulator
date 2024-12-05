f4a96f954d59de2bf1a45d28e5a7834281753364
lizheng-pc <lizheng-pc>
2024-12-05 23:21:04 +0800

    add test timeout middleware code

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github/leezone/simulator/middleware"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	engine := gin.Default()
+	engine.Use(middleware.Timeout)
 	engine.GET("/ping", func(ctx *gin.Context) {
+		time.Sleep(5 * time.Second)
 		ctx.Writer.WriteString("success")
 	})
 	engine.Run(":9001")
