b4bfc36397e73a73619877e0b3ff3bb5d92ed2b7
lizheng-pc <lizheng-pc>
2024-12-05 23:19:38 +0800

    add timeout middleware

---
+++ middleware/timeout.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"context"
+	"github/leezone/simulator/common/response"
+	"github/leezone/simulator/common/syserror"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TimeOut(ctx *gin.Context) {
+	timeoutCtx, cancel := context.WithTimeout(ctx, time.Second*3)
+	defer cancel()
+
+	success := make(chan struct{})
+	go func() {
+		ctx.Next()
+		close(success)
+	}()
+
+	select {
+	case <-timeoutCtx.Done():
+		response.Failure(ctx, syserror.TimeOutCode, syserror.ErrMap[syserror.TimeOutCode])
+		ctx.Abort()
+	case <-success:
+		// TODO,记录API操作日志
+		return
+	}
+}
