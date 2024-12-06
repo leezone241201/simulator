055fd86dc7df7a54830f1a41dab39bc94c7f076f
lizheng-pc <lizheng-pc>
2024-12-07 01:37:23 +0800

    add sqlite init log

--- common/db/sqlite.go
+++ common/db/sqlite.go
@@ -16,7 +16,8 @@ func InitSqlite(conf config.Sqlite) {
 	var err error
 	SqliteDB, err = gorm.Open(sqlite.Open(conf.Path))
 	if err != nil {
-		logger.Logger.ErrorWithStack("init database error", err, conf)
+		logger.Logger.ErrorWithStack("init sqlite error", err, conf)
 		os.Exit(-1)
 	}
+	logger.Logger.Info("init sqlite success")
 }
