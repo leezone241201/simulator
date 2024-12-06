a22ff144803189db5a634477bab74c9c89f57066
lizheng-pc <lizheng-pc>
2024-12-07 00:00:43 +0800

    add init config

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
-	"github/leezone/simulator/middleware"
+	"flag"
 	"time"
 
 	"github.com/gin-gonic/gin"
+
+	"github/leezone/simulator/common/db"
+	"github/leezone/simulator/common/logger"
+	"github/leezone/simulator/config"
+	"github/leezone/simulator/middleware"
 )
 
 func main() {
+	var configPath string
+	flag.StringVar(&configPath, "path", "config/conf.toml", "config path")
+	conf := config.NewConfig(configPath)
+
+	logger.InitLogger(conf.Logger)
+	db.InitSqlite(conf.Sqlite)
+
 	engine := gin.Default()
 	engine.Use(middleware.Timeout)
 	engine.GET("/ping", func(ctx *gin.Context) {
